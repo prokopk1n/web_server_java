345223127c8b96f685e2aef9e80b83cfc7f2fafe
prokopk1n <71408915+prokopk1n@users.noreply.github.com>
2021-03-24 13:25:14 +0300

    Add files via upload

--- src/test/java/TheatersTEST/TheatersTest.java
+++ src/test/java/TheatersTEST/TheatersTest.java
@@ -10,31 +10,28 @@ import java.util.Collection;
 
 public class TheatersTest {
     @Test
-    public void getAll(){
+    public void getAll() {
         boolean resultId1 = false;
         boolean resultId2 = false;
         TheatersDAO theatersDAO = new TheatersDAOImpl();
         Collection<Theaters> theaters = theatersDAO.getAll();
-        for (Theaters object: theaters)
-        {
+        for (Theaters object : theaters) {
             System.out.println(object.toString());
-            if (object.getTheater_id()==1){
+            if (object.getTheater_id() == 1) {
                 if (!resultId1)
                     resultId1 = true;
                 else {
                     resultId1 = false;
                     break;
                 }
-            }
-            else if (object.getTheater_id()==2){
+            } else if (object.getTheater_id() == 2) {
                 if (!resultId2)
                     resultId2 = true;
                 else {
                     resultId2 = false;
                     break;
                 }
-            }
-            else {
+            } else {
                 resultId1 = false;
                 break;
             }
@@ -43,15 +40,15 @@ public class TheatersTest {
     }
 
     @Test
-    public void getTheaterById(){
+    public void getTheaterById() {
         TheatersDAO theatersDAO = new TheatersDAOImpl();
-        Theaters theaters = theatersDAO.getObjectById((long)1);
-        Assert.assertTrue(theaters!=null && theaters.getTheater_id() == 1);
+        Theaters theaters = theatersDAO.getObjectById((long) 1);
+        Assert.assertTrue(theaters != null && theaters.getTheater_id() == 1);
     }
 
     @Test
-    public void addTheater(){
-        Theaters theater1 = new Theaters("name","email","address","phone","photo","map");
+    public void addTheater() {
+        Theaters theater1 = new Theaters("name", "email", "address", "phone", "photo", "map");
         TheatersDAO theatersDAO = new TheatersDAOImpl();
         theatersDAO.save(theater1);
         Theaters theater2 = theatersDAO.getObjectById(theater1.getTheater_id());
@@ -59,14 +56,72 @@ public class TheatersTest {
     }
 
     @Test
-    public void deleteTheater(){
-        Theaters theater1 = new Theaters("name","email","address","phone","photo","map");
+    public void deleteTheater() {
+        Theaters theater1 = new Theaters("name", "email", "address", "phone", "photo", "map");
         TheatersDAO theatersDAO = new TheatersDAOImpl();
         theatersDAO.save(theater1);
         long id = theater1.getTheater_id();
         theatersDAO.delete(theater1);
         Theaters theater2 = theatersDAO.getObjectById(id);
-        Assert.assertTrue(theater2==null);
+        Assert.assertTrue(theater2 == null);
+    }
+
+    @Test
+    public void updateTheater() {
+        TheatersDAO theatersDAO = new TheatersDAOImpl();
+        Theaters theaters = new Theaters("name", "email", "address", "phone", "photo", "map");
+        theatersDAO.save(theaters);
+        long id = theaters.getTheater_id();
+        theaters.setName("newName");
+        theatersDAO.update(theaters);
+        Assert.assertTrue(theatersDAO.getObjectById(id).getName().equals("newName"));
+    }
+
+    @Test
+    public void getTheaterByName(){
+        Theaters theater1 = new Theaters("testingName", "email", "address", "phone", "photo", "map");
+        Theaters theater2 = new Theaters("testingName", "email", "address", "phone", "photo", "map");
+        TheatersDAO theatersDAO = new TheatersDAOImpl();
+        theatersDAO.save(theater1);
+        theatersDAO.save(theater2);
+        Collection<Theaters> theaters = theatersDAO.getTheaterByName("testingName");
+        boolean result1 = false;
+        boolean result2 = false;
+        if (theaters.size()!=2) {
+            System.out.println(theaters.size());
+            Assert.assertTrue(false);
+            return;
+        }
+        for (Theaters object: theaters)
+        {
+            System.out.println(object);
+            if (object.getTheater_id()==theater1.getTheater_id())
+            {
+                if (!result1)
+                    result1 = true;
+                else
+                {
+                    result1 = false;
+                    break;
+                }
+            }
+            else if(object.getTheater_id()==theater2.getTheater_id())
+            {
+                if (!result2)
+                    result2 = true;
+                else
+                {
+                    result2 = false;
+                    break;
+                }
+            }
+            else
+            {
+                result1 = false;
+                break;
+            }
+        }
+        Assert.assertTrue(result1 && result2);
     }
 
 }
